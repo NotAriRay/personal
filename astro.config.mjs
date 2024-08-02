212936c4d3c177e83f4de56a2c9a2a39fd27f281
NotAriRay <arikarakushi@yahoo.com>
2024-08-02 21:46:53 +0200

    skibidi

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 export default defineConfig({
-  site: "https://tnixc.space",
+  site: "https://ariray.vercel.app",
   integrations: [tailwind(), sitemap()],
 });
